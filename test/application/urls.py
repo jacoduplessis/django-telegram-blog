121aca948c430e73c9b566de259f520c813ff73c
Jaco du Plessis <jaco@jacoduplessis.co.za>
2018-09-06 18:11:31 +0200

    Add urls to global namespace in test app

--- test/application/urls.py
+++ test/application/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from telegram_blog.urls import urlpatterns as app_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('telegram_blog.urls', namespace='telegram_blog'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + app_patterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
