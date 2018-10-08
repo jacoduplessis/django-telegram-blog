f0c242ae1e1e40f70473f24ac9b6edb4790a03c0
Jaco du Plessis <jaco@jacoduplessis.co.za>
2018-10-08 16:59:00 +0200

    Add url namespaces

--- test/application/urls.py
+++ test/application/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from telegram_blog.urls import urlpatterns as app_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('telegram_blog.urls'))
 ] + app_patterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
